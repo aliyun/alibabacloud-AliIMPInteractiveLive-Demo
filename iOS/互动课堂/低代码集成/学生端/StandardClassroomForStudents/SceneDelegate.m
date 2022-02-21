89b23c7c5d09348411b802bb94f9bcc8a089c154
君渡 <liuzaiyong.lzy@alibaba-inc.com>
2022-02-21 10:40:22 +0800

    iOS课堂低代码接入iPad学生端Demo增加简易登录界面

--- iOS/互动课堂/低代码集成/学生端/StandardClassroomForStudents/SceneDelegate.m
+++ iOS/互动课堂/低代码集成/学生端/StandardClassroomForStudents/SceneDelegate.m
@@ -24,7 +24,7 @@
     // iPhone互动课堂低代码学生端样式
     AIRBDClassroomStudent4iPhoneViewController* mainViewController = [[AIRBDClassroomStudent4iPhoneViewController alloc] init];
 //    // iPad互动课堂低代码学生端样式
-//    AIRBDClassroomStudent4iPadViewController* mainViewController = [[AIRBDClassroomStudent4iPadViewController alloc] initWithUserID:@"xxxxxxx" userNick:@"xxxx的昵称" classID:@"e4359547-4e31-478c-9fca-ec93cbe71b3c"];
+//    AIRBDClassroomStudent4iPadViewController* mainViewController = [[AIRBDClassroomStudent4iPadViewController alloc] init];
     UINavigationController *mainNavigationController = [[UINavigationController alloc] initWithRootViewController:mainViewController];
     mainNavigationController.navigationBarHidden = YES;
     mainNavigationController.navigationBar.translucent = NO;
