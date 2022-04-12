3225911219d88366569e5a3a9d0802ef9955a620
Kinice <sunzhaopeng.szp@alibaba-inc.com>
2022-04-12 20:17:05 +0800

    增加uniapp demo

---
+++ Web/uniapp-nvue-demo/App.vue
@@ -0,0 +1,16 @@
+<script>
+	import RoomEngine from './pages/RoomPaasSdk.uni.min.js'
+	export default {
+		globalData: {
+			appId: '****',
+			appKey: '****',
+			deviceId: '****', // 请确保每个设备都不同
+			roomEngine: RoomEngine.getInstance(),
+			origin: 'https://a.b.com'
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
