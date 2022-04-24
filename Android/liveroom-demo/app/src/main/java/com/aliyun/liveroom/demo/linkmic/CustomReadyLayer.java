d5a051c68baad4a1d43349cb21f961a64538323e
璞珂 <zijiao.wzj@alibaba-inc.com>
2022-04-24 15:27:08 +0800

    更改布局资源名称

--- Android/liveroom-demo/app/src/main/java/com/aliyun/liveroom/demo/linkmic/CustomReadyLayer.java
+++ Android/liveroom-demo/app/src/main/java/com/aliyun/liveroom/demo/linkmic/CustomReadyLayer.java
@@ -25,7 +25,7 @@ public class CustomReadyLayer extends FrameLayout implements ComponentHolder {
 
     public CustomReadyLayer(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        inflate(context, R.layout.view_linkmic_ready_layer, this);
+        inflate(context, R.layout.view_live_linkmic_ready_layer, this);
 
         findViewById(R.id.view_ready_switch_camera).setOnClickListener(v -> component.switchCamera());
         findViewById(R.id.view_ready_start_live).setOnClickListener(v -> component.startLive());
