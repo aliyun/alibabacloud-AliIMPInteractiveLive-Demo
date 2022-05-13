fce3405dca6eb686a8a526d4341a855560f8fd9e
玄竹 <xuanzhu.cg@alibaba-inc.com>
2022-05-13 20:06:45 +0800

    [update] #Flutter_sdk#   alicloud_impinteraction_sdk: ^1.8.6; view 注册下移

--- Flutter/sdk/android/app/src/main/java/com/aliyun/imp/flutter/sdk/alicloud_impinteraction_sdk_example/MainActivity.java
+++ Flutter/sdk/android/app/src/main/java/com/aliyun/imp/flutter/sdk/alicloud_impinteraction_sdk_example/MainActivity.java
@@ -8,13 +8,4 @@ import io.flutter.embedding.engine.FlutterEngine;
 import com.aliyun.imp.flutter.sdk.alicloud_impinteraction_sdk.NativeViewFactory;
 
 public class MainActivity extends FlutterActivity {
-
-    @Override
-    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-      super.configureFlutterEngine(flutterEngine);
-      flutterEngine
-              .getPlatformViewsController()
-              .getRegistry()
-              .registerViewFactory("<platform-view-type>", new NativeViewFactory());
-    }
 }
